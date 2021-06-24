500239e01749af2b21a70862eb82cd470e839a76
Martin Dahlén Jansson <martin@savelend.se>
2021-06-24 22:59:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php
 require 'bootstrap.php';
 
-$tf = new API();
+$status = [
+    'Unknown',
+    'Clear / Green',
+    'Minor Issue / Yellow',
+    'Significant Issue / Amber',
+    'Closed',
+];
 
-var_dump($tf->get_trail_status());
+
+$condition = [
+    'Unknown',
+    'Snow Groomed',
+    'Snow Packed',
+    'Snow Covered',
+    'Freeze/thaw Cycle',
+    'Icy',
+    'Prevalent Mud',
+    'Wet',
+    'Variable',
+    'Ideal',
+    'Dry',
+    'Very Dry',
+]
+
+
+$tf = new API();
+$ts = $tf->get_trail_status();
+var_dump($ts);
+
+echo $status[$ts->data->updates->trails_info->rows[0][1]];
+echo $condition[$ts->data->updates->trails_info->rows[0][2]];
