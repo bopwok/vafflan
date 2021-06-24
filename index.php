64d33c5e6c865d17f2abc80983779d7ef1841f3a
Martin Dahlén Jansson <martin@savelend.se>
2021-06-24 23:43:37 +0200

    updates

--- index.php
+++ index.php
@@ -37,7 +37,15 @@ $status_message = [
     'Jo',
     'Jovars',
     'Nej',
-    'Nej! låt bli',
+    'Nej!!! åk inte!',
+];
+
+$status_img = [
+    'vemvet.jpg',
+    'jo.jpg',
+    'jovars.jpg',
+    'nej.jpg',
+    'nejnej.jpg',
 ];
 
 $messages = [
@@ -50,5 +58,44 @@ $messages = [
         6 => 'Jo men ta de försiktit, schläfft i kurvan.',
     ]
 ];
+?>
 
-echo '<h1>'.$status_message[$ts_status].'</h1>';
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Är Våfflan Öppen?</title>
+
+  <script type="module">
+    document.documentElement.classList.remove('no-js');
+    document.documentElement.classList.add('js');
+  </script>
+
+<link rel="preconnect" href="https://fonts.googleapis.com"> 
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
+<link href="https://fonts.googleapis.com/css2?family=Teko:wght@500&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="/tailwind.css">
+
+  <meta property="og:title" content="Är Våfflan Öppen?">
+  <meta property="og:image" content="https://arvafflanoppen.nu/img/<?php echo $status_img[$ts_status]; ?>">
+  <meta property="og:locale" content="sv_SE">
+  <meta property="og:type" content="website">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="og:url" content="https://arvafflanoppen.nu">
+
+  <link rel="icon" href="/favicon.ico">
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <meta name="theme-color" content="#FF00FF">
+</head>
+
+<body>
+    <div class="h-full w-full bg-gray-900 flex items-center justify-center">
+    <h1 class="text-yellow-400 text-6xl"><?php echo $status_message[$ts_status]; ?></h1>
+    </div>
+</body>
+</html>
+
