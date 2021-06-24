f390f87743b92a89a82de28d54e473e9734ba989
Martin Dahlén Jansson <martin@savelend.se>
2021-06-24 22:51:17 +0200

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -6,9 +6,9 @@ use GuzzleHttp\Client;
 class API {
 
     protected $client;
-    protected $app_id = 60;
-    protected $app_secret = 'ef439afb1aa564b5';
-    protected $rid = 4571;
+    private $app_id = 60;
+    private $app_secret = 'ef439afb1aa564b5';
+    private $rid = 4571;
 
     function __construct() {
         $this->client = new Client([
@@ -20,7 +20,7 @@ class API {
     }
 
     function get_trail_status() {
-        $response = $this->client->request('GET', 'trail_status', ['timestamp'=>0, 'trailids'=>38169, 'app_id'=>$this->$app_id, 'app_secret'=>$this->app_secret]);
+        $response = $this->client->request('GET', 'trail_status', ['timestamp'=>0, 'trailids'=>38169, 'app_id'=>$this->app_id, 'app_secret'=>$this->app_secret]);
 
         return json_decode($response->getBody());
     }
