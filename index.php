4d2879226bb634c3116c44be7e8f4965cdc1239b
Martin Dahlén Jansson <martin@savelend.se>
2021-06-24 23:25:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,5 +29,26 @@ $condition = [
 $tf = new API();
 $ts = $tf->get_trail_status();
 
-echo '<h1>'.$status[$ts->data->updates->trails_info->rows[0][1]].'</h1>';
-echo '<h2>'.$condition[$ts->data->updates->trails_info->rows[0][2]].'</h2>';
+$ts_status = $ts->data->updates->trails_info->rows[0][1];
+$ts_condition = $ts->data->updates->trails_info->rows[0][2];
+
+$status_message = [
+    'Vet int´',
+    'Jo',
+    'Jovars',
+    'Nej',
+    'Nej! låt bli',
+];
+
+$messages = [
+    [
+        0 => 'Je vet int´'
+    ],
+    [
+        4 => 'Jo, men tat lugnt, de är ganska blött.',
+        5 => 'Jo men visst, de ä bar å åk.',
+        6 => 'Jo men ta de försiktit, schläfft i kurvan.',
+    ]
+];
+
+echo '<h1>'.$status_message[$ts_status].'</h1>';
